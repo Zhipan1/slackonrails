85b7fc5a4861a258da6b810aa4b078c82e269e36
Zhi Pan <zhipan@berkeley.edu>
2015-11-26 02:05:28 -0800

    fixed routes

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,15 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  def new
-    super
-  end
-
-  # POST /resource/sign_in
-  def create
-    super
-  end
 
   # DELETE /resource/sign_out
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,19 @@
 Rails.application.routes.draw do
 
 
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations'}
   resources :channels, :messages, :users
 
 
   get '/channels/:channel_id/leave', to: 'channels#user_leave'
   get '/channels/:channel_id/join', to: 'channels#user_join', as: 'join_channel'
 
+  authenticated :user do
+    root to: "home#index", as: 'authenticated'
+  end
+
   devise_scope :user do
     root to: "users/sessions#new"
-  end
-
-  authenticated :user do
-    root to: "home#index", :as => "profile"
-  end
-
-  unauthenticated do
-    root to: "users/registrations#new", :as => "unauthenticated"
   end
 
 
