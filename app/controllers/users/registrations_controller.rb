cc1b461b7b91e9fbf86dd2e8366a71edfa80ff15
Zhi Pan <zhipan@berkeley.edu>
2015-11-25 23:42:59 -0800

    updated session routes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 # before_filter :configure_sign_up_params, only: [:create]
 # before_filter :configure_account_update_params, only: [:update]
+  def new
+    super
+  end
 
   def create
     super
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :channels, :messages, :users
 
@@ -6,6 +8,17 @@ Rails.application.routes.draw do
   get '/channels/:channel_id/leave', to: 'channels#user_leave'
   get '/channels/:channel_id/join', to: 'channels#user_join', as: 'join_channel'
 
-  root to: "home#index"
+  devise_scope :user do
+    root to: "users/sessions#new"
+  end
+
+  authenticated :user do
+    root to: "home#index", :as => "profile"
+  end
+
+  unauthenticated do
+    root to: "users/registrations#new", :as => "unauthenticated"
+  end
+
 
 end
