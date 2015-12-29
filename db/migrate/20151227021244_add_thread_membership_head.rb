96bbe43d2b0440ca4f14b036dc667289c6677d82
Zhi Pan <zhipan@berkeley.edu>
2015-12-29 02:42:12 -0800

    lmao wtf had the wrong fucking reference for thread_membership.head. lmao dead af

--- db/migrate/20151227021244_add_thread_membership_head.rb
+++ db/migrate/20151227021244_add_thread_membership_head.rb
@@ -1,6 +1,6 @@
 class AddThreadMembershipHead < ActiveRecord::Migration
   def change
-    add_reference :thread_memberships, :head, references: :message_threads, index: true
-    add_foreign_key :thread_memberships, :message_threads, column: :head_id
+    add_reference :thread_memberships, :head, references: :messages, index: true
+    add_foreign_key :thread_memberships, :messages, column: :head_id
   end
 end
