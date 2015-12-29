3f2384fb73048d1adb62b609cf9fd4a09070407d
Zhi Pan <zhipan@berkeley.edu>
2015-12-29 00:37:45 -0800

    lmao wtf migration error'

--- db/migrate/20151220181257_add_message_to_thread.rb
+++ db/migrate/20151220181257_add_message_to_thread.rb
@@ -1,6 +1,6 @@
 class AddMessageToThread < ActiveRecord::Migration
   def change
     add_reference :messages, :message_thread, index: true
-    add_foreign_key :messages, :message_thread
+    add_foreign_key :messages, :message_threads
   end
 end
